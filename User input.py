68377c3eef5c79f5ae468ac89e0e1f8eec3e1bc1
SE0012 <deekshithaalisetty@my.unt.edu>
2022-11-30 19:42:08 -0600

    Create User input.py

---
+++ User input.py
@@ -0,0 +1,5 @@
+#defining the variables for taking the user input for the symptoms and testing the function using those variables 
+
+symptom1 =  str(input("please input the 1st symptom - "))
+symptom2 =  str(input("please input the 2nd symptom - "))
+symptom3 =  str(input("please input the 3rd symptom - "))

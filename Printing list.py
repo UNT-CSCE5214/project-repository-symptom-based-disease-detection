ca651797af516f368380a793b9ba6fa587fbf954
SE0012 <deekshithaalisetty@my.unt.edu>
2022-11-30 19:44:26 -0600

    Create Printing list.py

---
+++ Printing list.py
@@ -0,0 +1,7 @@
+sym_list = list()
+sym_list.append(symptom1)
+sym_list.append(symptom2)
+sym_list.append(symptom3)
+sym_list_string = str(",".join(sym_list))
+print(sym_list_string)
+# print(",".join(sym_list))
